8f7b39b8e89c86464ddab5cd8ac53bb2b07b7ffb
Forrest Jones <forrestjones2010@gmail.com>
2023-02-23 10:28:40 -0600

    GDC6

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,3 +1,8 @@
 def oxford_comma(items):
-    return None
-#GDC6
+    if len(items) <= 1:
+        return "".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
+#GPT REDO 2.23.23
